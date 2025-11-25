d229a902890e42395833552ee9a4fff79d8199aa
Stephen Maher <smaher500@gmail.com>
2025-11-25 00:19:14 +0000

    File changes

--- src/components/utils/songRecommendations.jsx
+++ src/components/utils/songRecommendations.jsx
@@ -1,4 +1,69 @@
 // Song recommendations based on numerology numbers
+
+// Daily songs based on universal day number
+export const DAILY_SONGS = {
+  1: [
+    { song: "Eye of the Tiger", artist: "Survivor", reason: "New beginnings energy" },
+    { song: "Roar", artist: "Katy Perry", reason: "Leadership day" },
+    { song: "Stronger", artist: "Kelly Clarkson", reason: "Independence power" }
+  ],
+  2: [
+    { song: "Lean on Me", artist: "Bill Withers", reason: "Partnership day" },
+    { song: "You've Got a Friend", artist: "Carole King", reason: "Cooperation energy" },
+    { song: "Better Together", artist: "Jack Johnson", reason: "Harmony vibes" }
+  ],
+  3: [
+    { song: "Happy", artist: "Pharrell Williams", reason: "Creative expression day" },
+    { song: "Walking on Sunshine", artist: "Katrina & The Waves", reason: "Joyful energy" },
+    { song: "Don't Stop Me Now", artist: "Queen", reason: "Self-expression power" }
+  ],
+  4: [
+    { song: "We Built This City", artist: "Starship", reason: "Foundation building day" },
+    { song: "Hard Work", artist: "Various", reason: "Discipline energy" },
+    { song: "Build Me Up Buttercup", artist: "The Foundations", reason: "Structure day" }
+  ],
+  5: [
+    { song: "Free Fallin'", artist: "Tom Petty", reason: "Freedom day" },
+    { song: "Born to Run", artist: "Bruce Springsteen", reason: "Adventure energy" },
+    { song: "Changes", artist: "David Bowie", reason: "Embrace change" }
+  ],
+  6: [
+    { song: "Greatest Love of All", artist: "Whitney Houston", reason: "Family/love day" },
+    { song: "Home", artist: "Edward Sharpe", reason: "Nurturing energy" },
+    { song: "Lean on Me", artist: "Bill Withers", reason: "Care for others" }
+  ],
+  7: [
+    { song: "The Sound of Silence", artist: "Simon & Garfunkel", reason: "Introspection day" },
+    { song: "Imagine", artist: "John Lennon", reason: "Spiritual wisdom" },
+    { song: "Hallelujah", artist: "Leonard Cohen", reason: "Deep reflection" }
+  ],
+  8: [
+    { song: "Empire State of Mind", artist: "Jay-Z", reason: "Achievement day" },
+    { song: "Money", artist: "Pink Floyd", reason: "Abundance energy" },
+    { song: "We Are the Champions", artist: "Queen", reason: "Success power" }
+  ],
+  9: [
+    { song: "Heal the World", artist: "Michael Jackson", reason: "Humanitarian day" },
+    { song: "Man in the Mirror", artist: "Michael Jackson", reason: "Completion energy" },
+    { song: "We Are the World", artist: "USA for Africa", reason: "Universal love" }
+  ],
+  11: [
+    { song: "Imagine", artist: "John Lennon", reason: "Master visionary day" },
+    { song: "Bohemian Rhapsody", artist: "Queen", reason: "Spiritual depth" },
+    { song: "Stairway to Heaven", artist: "Led Zeppelin", reason: "Illumination energy" }
+  ],
+  22: [
+    { song: "We Are the Champions", artist: "Queen", reason: "Master builder day" },
+    { song: "Dream On", artist: "Aerosmith", reason: "Manifest big dreams" },
+    { song: "Lose Yourself", artist: "Eminem", reason: "Build your legacy" }
+  ],
+  33: [
+    { song: "What a Wonderful World", artist: "Louis Armstrong", reason: "Master teacher day" },
+    { song: "Let It Be", artist: "The Beatles", reason: "Healing energy" },
+    { song: "Amazing Grace", artist: "Traditional", reason: "Divine guidance" }
+  ]
+};
+
 export const SONG_RECOMMENDATIONS = {
   // Life Path songs
   lifePath: {
